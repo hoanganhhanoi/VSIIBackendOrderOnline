05616afe6674203909454cfe1dc526362f3db994
unknown <anhnh2@anhnh2-w7.vsii.com>
2017-08-16 10:43:52 +0700

    edit root initializer

--- src/main/java/com/vsii/ServletInitializer.java
+++ src/main/java/com/vsii/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.vsii.initializer;
+package com.vsii;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
--- src/main/java/com/vsii/VsiiFoodApplication.java
+++ src/main/java/com/vsii/VsiiFoodApplication.java
@@ -1,4 +1,4 @@
-package com.vsii.initializer;
+package com.vsii;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
